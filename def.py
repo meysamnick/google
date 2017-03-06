d875a998bcafb5172d70b097cbf569e002bcaf51
meysamnick <sarabadani.meysam@gmail.com>
2017-03-06 22:21:17 +0100

    test3

---
+++ def.py
@@ -0,0 +1,17 @@
+def happyBirthdayEmily(): #program does nothing as written
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear Emily.")
+    print("Happy Birthday to you!")
+
+def happyBirthdayAndre():
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear Andre.")
+    print("Happy Birthday to you!")
+
+def main():
+    happyBirthdayAndre()
+    happyBirthdayEmily()
+
+main()
